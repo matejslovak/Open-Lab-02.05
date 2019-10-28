0e5011ba0cff900013ffae4faf5d0161996df8a7
matejslovak <joggurtik1@gmail.com>
2019-10-28 10:03:10 +0100

    f

---
+++ Open-Lab-02.05/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_02._05
+{
+    class Calculator
+    {
+        public bool DivisibleByFive(int number)
+        {
+            return (number % 5 == 0);
+  
+        }
+    }
+}
+
---
+++ Open-Lab-02.05/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_02._05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator number = new Calculator();
+            Console.WriteLine(number.DivisibleByFive(8));
+        }
+    }
+}
